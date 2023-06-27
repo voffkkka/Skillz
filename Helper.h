dfec92dd14eae6d44cfc1fcf8b53c3ecd2820417
Вовус <nikitin.vld@gmail.com>
2023-06-27 17:14:05 +0600

    HElper

---
+++ Helper.h
@@ -0,0 +1,11 @@
+#pragma once
+class Helper
+
+    int GetSquareOfSum(int a, int b)
+    {
+        int sum = a + b;
+        int square = sum * sum;
+        return square;
+    }
+;
+
---
+++ SKILLZ.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Helper.h"
+
+
+int main()
+{
+
+    int a = 2;
+    int b = 3;
+
+    int result = GetSquareOfSum(a, b);
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
